34eb36bd0a7ac4497cfceab895da9708f94e0ab0
lo kesh <mexican.dirtyfellow@gmail.com>
2017-07-26 10:22:19 +0530

    style: example updated

--- src/app/data/users.ts
+++ src/app/data/users.ts
@@ -1,151 +1,521 @@
-export const users = [{
-  'id': 1,
-  'name': 'Jsandye Hessle',
-  'email': 'jhessle0@hao123.com',
-  'gender': 'Male'
-}, {
-  'id': 2,
-  'name': 'Gifford Cullin',
-  'email': 'gcullin1@chron.com',
-  'gender': 'Female'
-}, {
-  'id': 3,
-  'name': 'Derk Broose',
-  'email': 'dbroose2@simplemachines.org',
-  'gender': 'Male'
-}, {
-  'id': 4,
-  'name': 'Winifred Lapenna',
-  'email': 'wlapenna3@msu.edu',
-  'gender': 'Male'
-}, {
-  'id': 5,
-  'name': 'Dayna MacGillespie',
-  'email': 'dmacgillespie4@rediff.com',
-  'gender': 'Female'
-}, {
-  'id': 6,
-  'name': 'Colet Ourtic',
-  'email': 'courtic5@walmart.com',
-  'gender': 'Male'
-}, {
-  'id': 7,
-  'name': 'Krysta Conneau',
-  'email': 'kconneau6@cnet.com',
-  'gender': 'Male'
-}, {
-  'id': 8,
-  'name': 'Jodi Palser',
-  'email': 'jpalser7@imdb.com',
-  'gender': 'Male'
-}, {
-  'id': 9,
-  'name': 'Dell Felstead',
-  'email': 'dfelstead8@netvibes.com',
-  'gender': 'Female'
-}, {
-  'id': 10,
-  'name': 'Ingelbert Lambal',
-  'email': 'ilambal9@photobucket.com',
-  'gender': 'Female'
-}, {
-  'id': 11,
-  'name': 'Alric Seamer',
-  'email': 'aseamera@imageshack.us',
-  'gender': 'Female'
-}, {
-  'id': 12,
-  'name': 'Ginni Somes',
-  'email': 'gsomesb@chicagotribune.com',
-  'gender': 'Female'
-}, {
-  'id': 13,
-  'name': 'Dania Spyer',
-  'email': 'dspyerc@wisc.edu',
-  'gender': 'Male'
-}, {
-  'id': 14,
-  'name': 'Padget Hearns',
-  'email': 'phearnsd@miibeian.gov.cn',
-  'gender': 'Female'
-}, {
-  'id': 15,
-  'name': 'Annmaria Frary',
-  'email': 'afrarye@census.gov',
-  'gender': 'Male'
-}, {
-  'id': 16,
-  'name': 'Desirae Collis',
-  'email': 'dcollisf@feedburner.com',
-  'gender': 'Female'
-}, {
-  'id': 17,
-  'name': 'Alica Slowan',
-  'email': 'aslowang@qq.com',
-  'gender': 'Male'
-}, {
-  'id': 18,
-  'name': 'Yolanthe Eisikowitz',
-  'email': 'yeisikowitzh@foxnews.com',
-  'gender': 'Male'
-}, {
-  'id': 19,
-  'name': 'Peirce Spinetti',
-  'email': 'pspinettii@opensource.org',
-  'gender': 'Male'
-}, {
-  'id': 20,
-  'name': 'Miltie Pfertner',
-  'email': 'mpfertnerj@wikia.com',
-  'gender': 'Female'
-}, {
-  'id': 21,
-  'name': 'Bastian Hefforde',
-  'email': 'bheffordek@usgs.gov',
-  'gender': 'Female'
-}, {
-  'id': 22,
-  'name': 'Greer Jeays',
-  'email': 'gjeaysl@a8.net',
-  'gender': 'Female'
-}, {
-  'id': 23,
-  'name': 'Randa Makin',
-  'email': 'rmakinm@freewebs.com',
-  'gender': 'Male'
-}, {
-  'id': 24,
-  'name': 'Sancho Lugg',
-  'email': 'sluggn@digg.com',
-  'gender': 'Male'
-}, {
-  'id': 25,
-  'name': 'Enrika Pirrone',
-  'email': 'epirroneo@theatlantic.com',
-  'gender': 'Female'
-}, {
-  'id': 26,
-  'name': 'Billi Stothart',
-  'email': 'bstothartp@wsj.com',
-  'gender': 'Male'
-}, {
-  'id': 27,
-  'name': 'Bartholemy Gircke',
-  'email': 'bgirckeq@wufoo.com',
-  'gender': 'Female'
-}, {
-  'id': 28,
-  'name': 'Tessi Canfer',
-  'email': 'tcanferr@skype.com',
-  'gender': 'Male'
-}, {
-  'id': 29,
-  'name': 'Devonna Bouzek',
-  'email': 'dbouzeks@prweb.com',
-  'gender': 'Male'
-}, {
-  'id': 30,
-  'name': 'Trevar Sandy',
-  'email': 'tsandyt@sitemeter.com',
-  'gender': 'Female'
-}];
+export const users = [
+
+  {
+    'gender': 'male',
+    'name': {
+      'title': 'monsieur',
+      'first': 'fabio',
+      'last': 'morel'
+    },
+    'location': {
+      'street': '7822 rue bossuet',
+      'city': 'aclens',
+      'state': 'glarus',
+      'postcode': 9865
+    },
+    'email': 'fabio.morel@example.com',
+    'login': {
+      'username': 'crazymeercat806',
+      'password': 'fortune',
+      'salt': 'XkMdYyPN',
+      'md5': '8099cc67a6c34eac0c73a9023ff7f57a',
+      'sha1': '18c5b458d45c38d031d622ab54b8e975c1493956',
+      'sha256': 'c206499254415787f1d3162624667ffbd5234a10312ff95c88265b67a7a199d3'
+    },
+    'dob': '1970-03-22 08:54:34',
+    'registered': '2013-04-14 12:35:51',
+    'phone': '(754)-344-8028',
+    'cell': '(838)-151-1881',
+    'id': {
+      'name': 'AVS',
+      'value': '756.LPRC.AVPK.35'
+    },
+    'picture': {
+      'large': 'https://randomuser.me/api/portraits/men/79.jpg',
+      'medium': 'https://randomuser.me/api/portraits/med/men/79.jpg',
+      'thumbnail': 'https://randomuser.me/api/portraits/thumb/men/79.jpg'
+    },
+    'nat': 'CH'
+  },
+  {
+    'gender': 'male',
+    'name': {
+      'title': 'mr',
+      'first': 'walter',
+      'last': 'lynch'
+    },
+    'location': {
+      'street': '1083 main street',
+      'city': 'ripon',
+      'state': 'east sussex',
+      'postcode': 'P9B 7SH'
+    },
+    'email': 'walter.lynch@example.com',
+    'login': {
+      'username': 'crazygorilla750',
+      'password': 'amsterdam',
+      'salt': '7ohgdRkC',
+      'md5': '0448fa74f7b5f379ed4414a00cb0d8d5',
+      'sha1': 'f5952c76a708b38b5fe14cde478960c809692a24',
+      'sha256': '0ce707261473a9ad36cc3c30eec40a9e17032f5f64cd0c5a70c2252ef9b94b04'
+    },
+    'dob': '1963-08-25 06:09:51',
+    'registered': '2013-12-01 03:12:18',
+    'phone': '017683 61129',
+    'cell': '0732-890-544',
+    'id': {
+      'name': 'NINO',
+      'value': 'NK 97 31 21 E'
+    },
+    'picture': {
+      'large': 'https://randomuser.me/api/portraits/men/58.jpg',
+      'medium': 'https://randomuser.me/api/portraits/med/men/58.jpg',
+      'thumbnail': 'https://randomuser.me/api/portraits/thumb/men/58.jpg'
+    },
+    'nat': 'GB'
+  },
+  {
+    'gender': 'male',
+    'name': {
+      'title': 'mr',
+      'first': 'jonatã',
+      'last': 'campos'
+    },
+    'location': {
+      'street': '3224 rua treze de maio ',
+      'city': 'são caetano do sul',
+      'state': 'minas gerais',
+      'postcode': 71392
+    },
+    'email': 'jonatã.campos@example.com',
+    'login': {
+      'username': 'silverfrog634',
+      'password': 'hannah',
+      'salt': 'rU6ImksM',
+      'md5': 'd4e8be66ea7c5997bea400591ba1feaa',
+      'sha1': '40eee924a9b8472e8b001cc9b19c0b064f8abfa1',
+      'sha256': 'a659d4de75ab98c1daab8c39b5979d2288a055141b081f3a8d795fb0551b76ae'
+    },
+    'dob': '1947-11-03 09:46:57',
+    'registered': '2016-07-13 02:12:23',
+    'phone': '(57) 0177-3943',
+    'cell': '(56) 0414-7218',
+    'id': {
+      'name': '',
+      'value': null
+    },
+    'picture': {
+      'large': 'https://randomuser.me/api/portraits/men/71.jpg',
+      'medium': 'https://randomuser.me/api/portraits/med/men/71.jpg',
+      'thumbnail': 'https://randomuser.me/api/portraits/thumb/men/71.jpg'
+    },
+    'nat': 'BR'
+  },
+  {
+    'gender': 'female',
+    'name': {
+      'title': 'ms',
+      'first': 'gema',
+      'last': 'flores'
+    },
+    'location': {
+      'street': '4224 calle de bravo murillo',
+      'city': 'lugo',
+      'state': 'navarra',
+      'postcode': 87309
+    },
+    'email': 'gema.flores@example.com',
+    'login': {
+      'username': 'yellowleopard872',
+      'password': 'skydive',
+      'salt': '6ah5sJJe',
+      'md5': 'adbe2c41a158a901bd9cd028fb015902',
+      'sha1': '0b54839980a0494f68fe99d1fd371a27b0fa84ae',
+      'sha256': '5572ade24c4bb47b317e0edebafab1095befe8b06e74bbaa78c99efd607ea5f2'
+    },
+    'dob': '1945-10-03 04:24:38',
+    'registered': '2012-09-18 01:40:49',
+    'phone': '922-098-231',
+    'cell': '698-867-492',
+    'id': {
+      'name': 'DNI',
+      'value': '14065866-N'
+    },
+    'picture': {
+      'large': 'https://randomuser.me/api/portraits/women/62.jpg',
+      'medium': 'https://randomuser.me/api/portraits/med/women/62.jpg',
+      'thumbnail': 'https://randomuser.me/api/portraits/thumb/women/62.jpg'
+    },
+    'nat': 'ES'
+  },
+  {
+    'gender': 'male',
+    'name': {
+      'title': 'mr',
+      'first': 'dwight',
+      'last': 'rose'
+    },
+    'location': {
+      'street': '2791 country club rd',
+      'city': 'fort collins',
+      'state': 'iowa',
+      'postcode': 38738
+    },
+    'email': 'dwight.rose@example.com',
+    'login': {
+      'username': 'crazyduck708',
+      'password': 'westside',
+      'salt': 'xdPnOLQL',
+      'md5': '3bca0ca921303c79479fbc62a8d12800',
+      'sha1': '51e9836d653443aa220a83b2c4395085e8c20def',
+      'sha256': 'b77dfc0603cef1d2e3da588c24036d22e5ca721fb8282d57bf5ae88ae73fb339'
+    },
+    'dob': '1955-05-18 16:57:42',
+    'registered': '2011-06-21 00:56:47',
+    'phone': '(695)-426-7486',
+    'cell': '(249)-355-5141',
+    'id': {
+      'name': 'SSN',
+      'value': '090-62-2713'
+    },
+    'picture': {
+      'large': 'https://randomuser.me/api/portraits/men/16.jpg',
+      'medium': 'https://randomuser.me/api/portraits/med/men/16.jpg',
+      'thumbnail': 'https://randomuser.me/api/portraits/thumb/men/16.jpg'
+    },
+    'nat': 'US'
+  },
+  {
+    'gender': 'male',
+    'name': {
+      'title': 'mr',
+      'first': 'anouar',
+      'last': 'bardoel'
+    },
+    'location': {
+      'street': '7249 jutfaseweg',
+      'city': 'hardenberg',
+      'state': 'drenthe',
+      'postcode': 57773
+    },
+    'email': 'anouar.bardoel@example.com',
+    'login': {
+      'username': 'lazycat777',
+      'password': 'maradona',
+      'salt': 'FmhyEWE3',
+      'md5': '224569b342fb478ce986760b307e0c7e',
+      'sha1': '7edd73192ee44f24e226bd1392de875058e8b107',
+      'sha256': '53c3c608a8ec7decb61fe87a368e376f431e028b4b9bb0e74c281bcf69c3c104'
+    },
+    'dob': '1964-08-07 06:19:57',
+    'registered': '2006-04-07 06:33:02',
+    'phone': '(266)-592-4237',
+    'cell': '(825)-267-9108',
+    'id': {
+      'name': 'BSN',
+      'value': '35927578'
+    },
+    'picture': {
+      'large': 'https://randomuser.me/api/portraits/men/98.jpg',
+      'medium': 'https://randomuser.me/api/portraits/med/men/98.jpg',
+      'thumbnail': 'https://randomuser.me/api/portraits/thumb/men/98.jpg'
+    },
+    'nat': 'NL'
+  },
+  {
+    'gender': 'male',
+    'name': {
+      'title': 'mr',
+      'first': 'darren',
+      'last': 'wells'
+    },
+    'location': {
+      'street': '3699 kingsway',
+      'city': 'leeds',
+      'state': 'west glamorgan',
+      'postcode': 'FF9H 3ZL'
+    },
+    'email': 'darren.wells@example.com',
+    'login': {
+      'username': 'bluegorilla433',
+      'password': 'tasty',
+      'salt': 'V6bQzaMP',
+      'md5': '152568fa486189025d778e3848739818',
+      'sha1': '683179fc07ede3ce3bb57a8e9da8e47737e125fe',
+      'sha256': '78e910216eef029281c9e0deb21c2bca321f980b73e110dabe9ac8da17ef7bfa'
+    },
+    'dob': '1972-04-15 02:23:02',
+    'registered': '2003-07-10 18:28:45',
+    'phone': '017683 11309',
+    'cell': '0714-368-726',
+    'id': {
+      'name': 'NINO',
+      'value': 'RM 24 22 97 V'
+    },
+    'picture': {
+      'large': 'https://randomuser.me/api/portraits/men/26.jpg',
+      'medium': 'https://randomuser.me/api/portraits/med/men/26.jpg',
+      'thumbnail': 'https://randomuser.me/api/portraits/thumb/men/26.jpg'
+    },
+    'nat': 'GB'
+  },
+  {
+    'gender': 'male',
+    'name': {
+      'title': 'mr',
+      'first': 'louis',
+      'last': 'lewis'
+    },
+    'location': {
+      'street': '1794 maraekakaho road',
+      'city': 'hamilton',
+      'state': 'canterbury',
+      'postcode': 96983
+    },
+    'email': 'louis.lewis@example.com',
+    'login': {
+      'username': 'orangebutterfly558',
+      'password': 'armada',
+      'salt': 'JL04JytI',
+      'md5': '127c7dc2323b8c0c2588e14c475b2262',
+      'sha1': '90b2c255b53be7e61c1b36ca4f9c868ede6d29cf',
+      'sha256': 'ea8b8ab39d4113064a67b3d274b153faade8e7612101c9bee332a39dbde8c131'
+    },
+    'dob': '1988-09-14 21:57:59',
+    'registered': '2015-12-08 19:21:49',
+    'phone': '(376)-484-8359',
+    'cell': '(543)-383-2919',
+    'id': {
+      'name': '',
+      'value': null
+    },
+    'picture': {
+      'large': 'https://randomuser.me/api/portraits/men/13.jpg',
+      'medium': 'https://randomuser.me/api/portraits/med/men/13.jpg',
+      'thumbnail': 'https://randomuser.me/api/portraits/thumb/men/13.jpg'
+    },
+    'nat': 'NZ'
+  },
+  {
+    'gender': 'female',
+    'name': {
+      'title': 'mademoiselle',
+      'first': 'eline',
+      'last': 'perez'
+    },
+    'location': {
+      'street': '1551 esplanade du 9 novembre 1989',
+      'city': 'vaux-sur-morges',
+      'state': 'graubünden',
+      'postcode': 4502
+    },
+    'email': 'eline.perez@example.com',
+    'login': {
+      'username': 'crazyrabbit921',
+      'password': 'gggg',
+      'salt': 'G9jj7LbQ',
+      'md5': 'd8bada4c7a5de82f746a00bf53ec316d',
+      'sha1': 'e562e091316c179b17bbbe2b95fa4447f362c8b7',
+      'sha256': 'af84c0b14a9aa98f5988ad28e5ef53e8171c2b8e811f4d67eb6d8491e6365ff9'
+    },
+    'dob': '1960-02-01 04:35:36',
+    'registered': '2012-03-22 10:43:44',
+    'phone': '(473)-391-2629',
+    'cell': '(278)-393-1287',
+    'id': {
+      'name': 'AVS',
+      'value': '756.YVQK.HOHH.19'
+    },
+    'picture': {
+      'large': 'https://randomuser.me/api/portraits/women/62.jpg',
+      'medium': 'https://randomuser.me/api/portraits/med/women/62.jpg',
+      'thumbnail': 'https://randomuser.me/api/portraits/thumb/women/62.jpg'
+    },
+    'nat': 'CH'
+  },
+  {
+    'gender': 'female',
+    'name': {
+      'title': 'miss',
+      'first': 'clara',
+      'last': 'kristensen'
+    },
+    'location': {
+      'street': '9592 skovbovej',
+      'city': 'allinge',
+      'state': 'syddanmark',
+      'postcode': 84384
+    },
+    'email': 'clara.kristensen@example.com',
+    'login': {
+      'username': 'orangeswan575',
+      'password': 'novell',
+      'salt': 'LT8OrsE1',
+      'md5': '4c5d3b1e154d417088f6489e3f494a5a',
+      'sha1': '43914e2d6bdab09fd7042634eede8a2c87162999',
+      'sha256': 'ba01974918a57dd6e4804aeabfba0fe5b16d72cc4f1cccd6ec5e15a95338d661'
+    },
+    'dob': '1947-08-22 13:36:45',
+    'registered': '2011-05-18 17:42:48',
+    'phone': '31022883',
+    'cell': '69520388',
+    'id': {
+      'name': 'CPR',
+      'value': '144923-9900'
+    },
+    'picture': {
+      'large': 'https://randomuser.me/api/portraits/women/95.jpg',
+      'medium': 'https://randomuser.me/api/portraits/med/women/95.jpg',
+      'thumbnail': 'https://randomuser.me/api/portraits/thumb/women/95.jpg'
+    },
+    'nat': 'DK'
+  },
+  {
+    'gender': 'female',
+    'name': {
+      'title': 'mrs',
+      'first': 'debbie',
+      'last': 'garrett'
+    },
+    'location': {
+      'street': '7670 albert road',
+      'city': 'letterkenny',
+      'state': 'roscommon',
+      'postcode': 18157
+    },
+    'email': 'debbie.garrett@example.com',
+    'login': {
+      'username': 'bluegoose153',
+      'password': 'phillips',
+      'salt': 'k3XP85So',
+      'md5': '9120873f70c63dc825a998ea88f0811e',
+      'sha1': '9455edcb049b72180a5278ec1c4f3c0a8ae5520a',
+      'sha256': '0ccbef55a9204355a4e95215e64c2b69af545fe54e2e5e9e6ab178af0d6dbae5'
+    },
+    'dob': '1952-08-28 23:10:54',
+    'registered': '2014-01-31 11:38:47',
+    'phone': '031-885-7930',
+    'cell': '081-954-3595',
+    'id': {
+      'name': 'PPS',
+      'value': '1044284T'
+    },
+    'picture': {
+      'large': 'https://randomuser.me/api/portraits/women/5.jpg',
+      'medium': 'https://randomuser.me/api/portraits/med/women/5.jpg',
+      'thumbnail': 'https://randomuser.me/api/portraits/thumb/women/5.jpg'
+    },
+    'nat': 'IE'
+  },
+  {
+    'gender': 'male',
+    'name': {
+      'title': 'mr',
+      'first': 'james',
+      'last': 'ross'
+    },
+    'location': {
+      'street': '3145 maple ave',
+      'city': 'waterloo',
+      'state': 'manitoba',
+      'postcode': 72574
+    },
+    'email': 'james.ross@example.com',
+    'login': {
+      'username': 'lazybear662',
+      'password': 'neon',
+      'salt': 'K8c3nuT0',
+      'md5': 'c863dd347a72c1b9903163c1238aade5',
+      'sha1': '1616138dfde797d295bb056af88fe0e7a5936233',
+      'sha256': 'a048e0dec4d65541e9869f6f56d0f75a2b1ae8a8e810a44a869e09a8ac4be407'
+    },
+    'dob': '1986-01-05 07:19:15',
+    'registered': '2006-05-29 19:57:28',
+    'phone': '058-843-9552',
+    'cell': '903-492-1287',
+    'id': {
+      'name': '',
+      'value': null
+    },
+    'picture': {
+      'large': 'https://randomuser.me/api/portraits/men/10.jpg',
+      'medium': 'https://randomuser.me/api/portraits/med/men/10.jpg',
+      'thumbnail': 'https://randomuser.me/api/portraits/thumb/men/10.jpg'
+    },
+    'nat': 'CA'
+  },
+  {
+    'gender': 'male',
+    'name': {
+      'title': 'mr',
+      'first': 'emilio',
+      'last': 'marin'
+    },
+    'location': {
+      'street': '6391 calle de la luna',
+      'city': 'hospitalet de llobregat',
+      'state': 'castilla y león',
+      'postcode': 65701
+    },
+    'email': 'emilio.marin@example.com',
+    'login': {
+      'username': 'silverbutterfly570',
+      'password': 'hotmail1',
+      'salt': 'ZCKxdORp',
+      'md5': 'daa224923a50b457d3d2ca141066bdcd',
+      'sha1': '272faa16cb8b96fed84cb3492762a8876cc4c5d9',
+      'sha256': 'd77e6493b70c793fb0a33c162137a3b4b7ad942de987336e265ef5f948f22669'
+    },
+    'dob': '1982-03-02 07:14:04',
+    'registered': '2003-01-03 17:14:51',
+    'phone': '969-433-344',
+    'cell': '693-148-209',
+    'id': {
+      'name': 'DNI',
+      'value': '97438297-M'
+    },
+    'picture': {
+      'large': 'https://randomuser.me/api/portraits/men/82.jpg',
+      'medium': 'https://randomuser.me/api/portraits/med/men/82.jpg',
+      'thumbnail': 'https://randomuser.me/api/portraits/thumb/men/82.jpg'
+    },
+    'nat': 'ES'
+  },
+  {
+    'gender': 'female',
+    'name': {
+      'title': 'ms',
+      'first': 'pinja',
+      'last': 'marttila'
+    },
+    'location': {
+      'street': '5489 hatanpään valtatie',
+      'city': 'tuusniemi',
+      'state': 'southern savonia',
+      'postcode': 41021
+    },
+    'email': 'pinja.marttila@example.com',
+    'login': {
+      'username': 'smallswan673',
+      'password': 'talon',
+      'salt': 'ecBzL61W',
+      'md5': '6e4fe6a66cb2115efc1fedbdd00b70d2',
+      'sha1': '1ffd2edd931af480d6038356165fdf9eb690e4ac',
+      'sha256': 'be358d156620cec71490edc47b0502340b0fd90a09d41c5128c7d9937d32dd94'
+    },
+    'dob': '1973-01-06 00:26:43',
+    'registered': '2010-05-03 17:32:46',
+    'phone': '04-142-990',
+    'cell': '044-141-00-98',
+    'id': {
+      'name': 'HETU',
+      'value': '673-4528'
+    },
+    'picture': {
+      'large': 'https://randomuser.me/api/portraits/women/37.jpg',
+      'medium': 'https://randomuser.me/api/portraits/med/women/37.jpg',
+      'thumbnail': 'https://randomuser.me/api/portraits/thumb/women/37.jpg'
+    },
+    'nat': 'FI'
+  }
+];
